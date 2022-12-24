c21f9860f4c3e35e34018fbeeca4ab9673d82bfd
NubScript7 <112324399+NubScript7@users.noreply.github.com>
2022-12-24 13:54:40 +0800

    Add files via upload

---
+++ dbControlled/main.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+import { getDatabase,get,child,ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+const txt = document.querySelector('.comm');
+let firebaseConfig;
+await fetch("https://nubscript7.github.io/json-placeholder/firebase/config.json")
+.then(res=>res.json())
+.then(json=>firebaseConfig=json)
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getDatabase();
+setInterval(()=>{
+get(child(ref(db),"dbCon/htmlContent/value"))
+.then(msg=>txt.innerHTML = msg.val())
+.catch(error=>txt.textContent=error)
+},500)
